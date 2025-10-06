e95ce06df98a5ebd907f22b68c093caca64abba3
KARAN-SHARXA <karansharma2302578@gmail.com>
2025-10-06 17:26:22 +0530

    First and Last Occurrence of an Element in a Sorted Array

---
+++ array/FLaccurance.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+int firstOcc(int arr[], int n, int key)
+{
+  int start = 0;
+  int end = n - 1;
+  int mid = start + (end - start) / 2;
+  int ans = -1;
+
+  while (start <= end)
+  {
+    if (arr[mid] == key)
+    {
+      ans = mid;
+      end = mid - 1;
+    }
+    else if (key > arr[mid])
+    {
+      // Right part
+      start = mid + 1;
+    }
+    else if (key < arr[mid])
+    {
+      // left side
+      end = mid - 1;
+    }
+
+    mid = start + (end - start) / 2;
+  }
+  return ans;
+}
+
+int LastOcc(int arr[], int n, int key)
+{
+  int start = 0;
+  int end = n - 1;
+  int mid = start + (end - start) / 2;
+  int ans = -1;
+
+  while (start <= end)
+  {
+    if (arr[mid] == key)
+    {
+      ans = mid;
+      start = mid + 1;
+    }
+    else if (key > arr[mid])
+    {
+      // Right part
+      start = mid + 1;
+    }
+    else if (key < arr[mid])
+    {
+      // left side
+      end = mid - 1;
+    }
+
+    mid = start + (end - start) / 2;
+  }
+  return ans;
+}
+int main()
+{
+  int even[5] = {1, 2, 3, 3, 5};
+  cout << " First Occurrence of 3 is at " << firstOcc(even, 5, 3) <<endl;
+  cout << " last Occurrence of 3 is at " << LastOcc(even, 5, 3) <<endl;
+
+  return 0;
+}
